1bf0e01d81a4d7624bec78966d85ba6fc851c3f3
Herimanjaka Degany <geit.herimanjaka@gmail.com>
2025-04-04 20:42:22 +0300

    refactor (auth): ajout de contexte react pour l'authentification

---
+++ src/components/AuthContextProvider.jsx
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { 
+  createUser,
+  loginWithEmail,
+  getCurrentUser,
+  logout 
+} from '../services/authService';
+
+export const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+        setLoading(true)
+      try {
+        const currentUser = await getCurrentUser();
+        setUser(currentUser);
+      } catch (err) {
+        throw new Error(err.message)
+      }finally{
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, []);
+
+  const signup = async (email, password, name, profileImage = null) => {
+    try {
+      await createUser(email, password, name, profileImage);
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  };
+
+  const login = async (email, password) => {
+    try {
+      const currentUser = await loginWithEmail(email, password);
+      setUser(currentUser);
+    } catch (err) {
+        throw new Error(err.message);
+    }
+  };
+
+  const logoutUser = async () => {
+    try {
+      await logout();
+      setUser(null);
+    } catch (err) {
+        throw new Error(err.message);
+    }
+  };
+
+  const value = {
+    user,
+    loading,
+    signup,
+    login,
+    logout: logoutUser,
+    isAuthenticated: !!user
+  };
+
+  return (
+    <AuthContext value={value}>
+      {children}
+    </AuthContext>
+  );
+};
