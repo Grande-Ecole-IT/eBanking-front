136669094ee6fde7378b389ce9854e3569a91f1b
Herimanjaka Degany <geit.herimanjaka@gmail.com>
2025-04-04 22:06:02 +0300

    feat (auth) : protect routes dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Dashboard from "./pages/Dashboard";
 import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { ProtectedRoute } from "./components/ProtectedRoute";
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
